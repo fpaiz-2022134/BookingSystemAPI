ae053f4ebfe5f5ce99fd2869fc167c64ff5df0d7
fpaiz-2022134 <fpaiz-2022134@kinal.edu.gt>
2024-09-19 10:44:50 -0600

    Initial commit with HealthController

---
+++ src/main/java/com/example/bookingSystemAPI/BookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookingSystemAPI/BookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
